9f705939f0d1f3a152949287dcb89c030f5a5ef4
Furkan Özay <furkanozay0@gmail.com>
2021-05-26 02:25:32 +0300

    Readme Dosyası güncellendi. Projeler listelenmeye hazır hale getirildi.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# APP
-My demo projects
+# Temel Javascirpt Uygulamaları
+## Uygulamalar
+
+<ul>
+    <li>Javascript Basit Hesap Makinası</li>
+</ul>
+<hr>
+<br>
+
+### Basit Javascript Hesap Makinası
+Projede temel olarak Javascript for döngüsü, fonksiyon yapısı, HTML'e erişme ve Event kullanılmaya çalışılmıştır. 20 Satır Javascript kodu yazılmıştır. Input üzerine string değer yazılabilmesi ve diğer complex hesaplama işlemlerinin olmaması gibi hatalar en kısa sürede düzeltilecektir.
+<br><br>
+_Projeyi çalıştırmak için sadece bilgisayarınıza indirmeniz ve index.html dosyasını açamanız yeterli olacaktır._
--- calculator/custom.js
+++ calculator/custom.js
@@ -2,29 +2,21 @@ const buton = document.getElementsByTagName('button')
 const hesapla = document.getElementById('calculate')
 const sifirla = document.getElementById('reset')
 const sonuc = document.getElementById('result')
-
-
 function islem(){
     sonuc.value = eval(sonuc.value)
 }
-
 hesapla.addEventListener('click', islem)
-
-
 function yazdir(){
     sonuc.value = sonuc.value + this.value
 }
-
 for(let i = 0; i <= 15 ; i++){
     if(i != 12 && i != 14){
         buton[i].addEventListener('click', yazdir)
     }
 }
-
 function temizle(){
     sonuc.value = ""
 }
-
 sifirla.addEventListener('click', temizle)
 
 
